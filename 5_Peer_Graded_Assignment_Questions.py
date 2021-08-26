926e0e1fde213860253bb919354a0b7f755423a6
asinghhtx <80661111+asinghhtx@users.noreply.github.com>
2021-08-25 23:33:26 -0500

    Update 5_Peer_Graded_Assignment_Questions.py

--- 5_Peer_Graded_Assignment_Questions.py
+++ 5_Peer_Graded_Assignment_Questions.py
@@ -74,37 +74,35 @@ def compute_data_choice_2(df):
 app.layout = html.Div(children=[ 
                                 # TASK1: Add title to the dashboard
                                 # Enter your code below. Make sure you have correct formatting.
-                                html.H1('US Domestic Airline Flights Performance', 
-                                        style={'textAlign': 'center',
-                                                'color': '#503D36',
-                                                'font-size': 24}),
-
+                                html.H1('US Domestic Airline Flights Performance',
+                                    style={'textAlign': 'center',
+                                    'color': '#503D36',
+                                    'font-size': 24}
+                                    ),
                                 # REVIEW2: Dropdown creation
                                 # Create an outer division 
-                                html.Div(
-                                    [
+                                html.Div([
                                     # Add an division
                                     html.Div([
                                         # Create an division for adding dropdown helper text for report type
-                                        html.Div([
+                                        html.Div(
+                                            [
                                             html.H2('Report Type:', style={'margin-right': '2em'}),
-                                        ]),
+                                            ]
+                                        ),
+
                                         # TASK2: Add a dropdown
                                         # Enter your code below. Make sure you have correct formatting.
-                                        dcc.Dropdown(id='Input-type',
-                                                    options=[
-                                                    {'label': 'Yearly Airline Performance Report', 'value': 'OPT1'},
-                                                    {'label': 'Yearly Airline Delay Report', 'value': 'OPT2'}],
-                                                    placeholder='Select a report type',
-                                                    style={'width':'80%', 
-                                                            'padding':'3px', 
-                                                            'font-size': '20px', 
-                                                            'text-align-last' : 'center'}
-                                         ),
-
+                                        dcc.Dropdown(id='input-type',
+                                                options=[
+                                                        {'label': 'Yearly Airline Performance Report', 'value': 'OPT1'},
+                                                        {'label': 'Yearly Airline Delay Report', 'value': 'OPT2'}
+                                                         ],
+                                                placeholder='Select a report type',
+                                                style={'width':'80%', 'padding': '3px', 'font-size': '20px', 'text-align-last' : 'center'}
+                                        ),
                                     # Place them next to each other using the division style
-                                    ], style={'display':'flex'}
-                                    ),
+                                    ], style={'display':'flex'}),
                                     
                                    # Add next division 
                                    html.Div([
@@ -112,20 +110,16 @@ app.layout = html.Div(children=[
                                         html.Div(
                                             [
                                             html.H2('Choose Year:', style={'margin-right': '2em'})
-                                        ]),
+                                            ]
+                                        ),
                                         dcc.Dropdown(id='input-year', 
                                                      # Update dropdown values using list comphrehension
                                                      options=[{'label': i, 'value': i} for i in year_list],
                                                      placeholder="Select a year",
-                                                     style={'width':'80%', 
-                                                            'padding':'3px', 
-                                                            'font-size': '20px', 
-                                                            'text-align-last' : 'center'}
-                                                    ),
+                                                     style={'width':'80%', 'padding':'3px', 'font-size': '20px', 'text-align-last' : 'center'}),
                                             # Place them next to each other using the division style
-                                        ], style={'display': 'flex'}
-                                    ),  
-                                 ]),
+                                            ], style={'display': 'flex'}),  
+                                          ]),
                                 
                                 # Add Computed graphs
                                 # REVIEW3: Observe how we add an empty division and providing an id that will be updated during callback
@@ -134,29 +128,24 @@ app.layout = html.Div(children=[
                                 html.Div([
                                         html.Div([ ], id='plot2'),
                                         html.Div([ ], id='plot3')
-                                 ], style={'display': 'flex'}
-                                ),
+                                ], style={'display': 'flex'}),
                                 
                                 # TASK3: Add a division with two empty divisions inside. See above disvision for example.
                                 # Enter your code below. Make sure you have correct formatting.
                                html.Div([
                                         html.Div([ ], id='plot4'),
                                         html.Div([ ], id='plot5')
-                                ], style={'display': 'flex'}
-                                ),
-              ])
-
-#                              ])
+                               ], style={'display': 'flex'})
+                                ])
 
 # Callback function definition
 # TASK4: Add 5 ouput components
 # Enter your code below. Make sure you have correct formatting.
-
-@app.callback( [Output (component_id='plot1', component_property='children'),
-                Output (component_id='plot2', component_property='children'),
-                Output (component_id='plot3', component_property='children'),
-                Output (component_id='plot4', component_property='children'),
-                Output (component_id='plot5', component_property='children')],
+@app.callback( [Output(component_id='plot1', component_property='children'),
+                Output(component_id='plot2', component_property='children'),
+                Output(component_id='plot3', component_property='children'),
+                Output(component_id='plot4', component_property='children'),
+                Output(component_id='plot5', component_property='children')],
                [Input(component_id='input-type', component_property='value'),
                 Input(component_id='input-year', component_property='value')],
                # REVIEW4: Holding output state till user enters all the form information. In this case, it will be chart type and year
@@ -179,7 +168,7 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
             
             # TASK5: Average flight time by reporting airline
             # Enter your code below. Make sure you have correct formatting.
-            line_fig = px.line(line_data, x='Month', y='Airtime', color='Reporting_Airline', title='Average Monthly Flight time (minutes) by Airline')
+            line_fig = px.line(line_data, x='Month', y='AirTime', color='Reporting_Airline', title='Average Monthly Flight Time (minutes) by Airline')
             
             # Percentage of diverted airport landings per reporting airline
             pie_fig = px.pie(div_data, values='Flights', names='Reporting_Airline', title='% of flights by reporting airline')
@@ -198,13 +187,12 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
             
             # TASK6: Number of flights flying to each state from each reporting airline
             # Enter your code below. Make sure you have correct formatting.
-            tree_fig = px.treemap(tree_data ,
+            tree_fig = px.treemap(tree_data, 
                         path=['DestState', 'Reporting_Airline'],
-                        values='Flights' ,
-                        color='Flights' ,
-                        color_continuous_scale='RdBu' ,
-                        title='Flight Count by Airline to Destination State'
-                        )
+                        values='Flights',
+                        color='Flights',
+                        color_continuous_scale='RdBu',
+                        title='Flight count by Airline to Destination State')
             
             
             # REVIEW6: Return dcc.Graph component to the empty division
@@ -236,3 +224,9 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
 # Run the app
 if __name__ == '__main__':
     app.run_server()
+    
+    #Run this on Terminal 
+    #pip3 install pandas dash
+    #run this file using Python
+    #python3 5_Peer_Graded_Assignment_Questions.py
+    #once done, launch Application and use the port 8050 to launch the dash. 
